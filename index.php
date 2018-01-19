c66980e6da9b9edc6a16c9820d55e08bb8949c4a
Francois <francoischpn@gmail.com>
2018-01-19 18:57:47 +0100

    yeah

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+  require 'header.php';
+
+?>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <h1>Accueil</h1></br></br>
+    <div class="col-sm-4 s">
+      <p>Passioné d'apiculture, j'élève des abeilles et produis du miel en seine et marne depuis 2001.
+          Je suis installé dans le village de La Haute Maison entre les villes de Crécy La Chapelle, Meaux, Coulommiers et La ferté sous Jouarre.
+          Je possède également des ruches sur les hauteurs de Crécy La Chapelle mais aussi en bordure de la forêt régionale de Ferrières.
+
+      </p>
+    </div>
+    <div class="col-sm-4 s">
+      <img src="images/img/Homme.jpg" width="200" height="150" class="d-inline-block align-top" alt="">
+    </div>
+      <hr>
+    </div>
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid text-center">
+
+<?php
+  require 'footer.php';
+?>
