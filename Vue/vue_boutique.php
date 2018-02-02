98b7e30b46f0d69d9b76e5979af7ef8de4aace28
Francois <francoischpn@gmail.com>
2018-02-02 13:56:55 +0100

    modif vue boutique

--- Vue/vue_boutique.php
+++ Vue/vue_boutique.php
@@ -1,41 +1,58 @@
+
+
+
 <div class="container-fluid text-center">    
   <div class="row content">
-    <div class="col-sm-2 sidenav">
+    <div class="col-md-2 sidenav">
     </div>
-    <div class="col-sm-8 text-left"> 
+      <div class="col-sm-8 text-left"> 
       <h1>Boutique</h1>
        <hr>
 
+          <div class="container">    
+            <div class="row">
+              <div class="col-md-4">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">Miel de merde</div>
+                  <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                  <div class="panel-footer">Un bon miel </div>
+                </div>
+              </div>
 
-     <?php 
-    //Boucle pour afficher les images
-     while ($donnees = $reponse->fetch()) {
-          echo '<img src="../images/Produit/'.$donnees['IMGP'].'.jpg" width="150" height="150" class="d-inline-block align-top" alt=""></br></br>
-          <label>'.$donnees['PRIXP'].'euro </label><button>Ajouter au panier</button>';
-        
-       // echo "</div>";
-      }
-      ?>    
-    </div>
+              <div class="col-md-4">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">Miel de merde</div>
+                  <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                  <div class="panel-footer">Un bon miel </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">Miel de merde</div>
+                  <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                  <div class="panel-footer">Un bon miel <button>Ajouter au panier</button></div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">Miel de merde</div>
+                  <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                  <div class="panel-footer">Un bon miel </div>
+                </div>
+              </div>
+
+              
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2 sidenav">
     
-    <div class="col-sm-2 sidenav">
-    </div>
-  </div>
-</div>
 
-<div class="container-fluid text-center">    
-  <div class="row content">
-    <div class="col-sm-2 sidenav">
-      </div>
-      <div class="col-sm-8">
-        <h1>A propos</h1>
-      </div>
-      <div class="col-sm-2 sidenav">
-      </div>
-  </div>
+      
+        </div>
+    </div>
+</div>
 </div>
 
       
-      
-    
-
