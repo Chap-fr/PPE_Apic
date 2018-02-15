d9ccbf924405a5273243a4a6151db312138dde20
LpTone <luc.antoine.lepape@gmail.com>
2018-02-15 15:46:26 +0100

    Add files via upload

--- Controleur/Control_boutique.php
+++ Controleur/Control_boutique.php
@@ -1,18 +1,38 @@
 <?php
-/*
-
-class Boutique{
-
-
-public function setTableBoutique{
-
-
-
-	$requete = "SELECT  * FROM Produit";
-	$reponse = $bdd->query($requete);
+require './Modele/model.produit.php';
+	class Control_produit
+	{
+		private $unModele;
+		
+		public function __construct ($serveur, $bdd, $user, $mdp)
+		{
+			$this->unModele = new ModeleProduit($serveur, $bdd, $user, $mdp);
+			$this->unModele->setTable("produit");
+		}
+		
+		public function selectAll()
+		{
+			return $this->unModele->selectAll();
+			
+		}
+		public function selectWhere ($champs, $where) {
+			return $this->unModele->selectWhere($champs, $where);
+		}
+		
+		public function insert($tab){
+			$this->unModele->insert($tab);
+		}
+		
+		public function delete ($where) {
+			$this->unModele->delete($where);
+		}
+		
+		public function update($tab, $where) {
+			$this->unModele->update($tab,$where);
+		}
+		
 	}
-	
-
-
-}*/
+$unControleur = new Control_produit("localhost","bdd_ppe","root","");
+$donnees = $unControleur->selectAll();
 require './Vue/vue_boutique.php';
+?>
